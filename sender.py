d0dcd1e0f6bd119f9cc448a3c58ce79a8fbf4e1e
Sam Yamoah <sya57@uclive.ac.nz>
2016-08-24 19:17:28 +1200

    Added error checking for invalid command input

--- sender.py
+++ sender.py
@@ -1,9 +1,33 @@
+#!/usr/bin/python
+import sys
 import socket
 
 '''
 Sender Sockets: s_in and s_out
 
 '''
+#valid Port no.
+VALID_PORTS =  range(1024, 64001)
+
+# Get the arguments list
+args = (sys.argv)
+#cmdargs.pop(0)
+
+#try:
+if len(cmdargs) == 5:
+    for port in args[1:-1]:
+        if int(port) not in VALID_PORTS:
+            #HAVE A TRY EXCEPTION
+            print("port {} not a valid port".format(port))
+else:
+    print("Inut ERROR")
+#raise Exception("Invalid number of parameters")
+
+
+
+
+
+
 s_in = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s_in.connect(("localhost", 1234))
