cca80135bf87fac51fa47f1bf418a656bc5ca948
Sam Yamoah <sya57@uclive.ac.nz>
2016-09-02 19:32:54 +1200

    Started working on the packet object

---
+++ packet.py
@@ -0,0 +1,12 @@
+class Packet():
+    def __init__(self, magicno, packet_type, seqno, dataLen, data):
+        self.magicno = int(magicno)
+        self.packet_type = packet_type
+        self.seqno = seqno
+        self.dataLen = dataLen
+        self.data = data
+
+
+    # def __str__(self):
+    #     '''Returns all data in one string with the packet info at the head'''
+    #     return "{}{}{}{}{}\\".format(str)
