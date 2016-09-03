93b3ed1384c1e976d3a65e6dadd8d1733369bcb2
Sam Yamoah <sya57@uclive.ac.nz>
2016-09-03 15:20:02 +1200

    added more to the run script, cant remember what though

--- basic/run.sh
+++ basic/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-gnome-terminal -e reciever_basic.py 1234 1235 sender.txt
-gnome-terminal -e sender_basic.py 1234 1235 input.txt
+gnome-terminal -e python3 "reciever_basic.py 1234 1235 sender.txt"
+gnome-terminal -e python3 "sender_basic.py 1234 1235 input.txt"
 
